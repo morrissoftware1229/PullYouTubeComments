82b75b3324a5c9bde9d181a14042918f33d58e64
Charles Morris <morrissoftware1229@gmail.com>
2023-02-15 00:44:21 -0600

    Adjusted loop to feed in nextPageToken and pull first 400 comments

--- PullYouTubeComments.py
+++ PullYouTubeComments.py
@@ -18,24 +18,53 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = "InsertAPIKeyHere"
+    DEVELOPER_KEY = "InsertAPIKeyBeforeNextUse"
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
+    #dictionary to hold API returns
+    returnedValues = []
+
+    #Calling the API the first time
     request = youtube.commentThreads().list(
         part="snippet,replies",
-        videoId="l1LdFUFFRBQ"
+        videoId="l1LdFUFFRBQ",
+        pageToken=None
     )
     response = request.execute()
+    returnedValues.append(response)
+    nextPageToken = response['nextPageToken']
+
+    #Calling the API subsequent times
+    #Does YouTube Data API limit to 400 comments?
+    maxPage = 19
+    for x in range(0, maxPage):
+        request = youtube.commentThreads().list(
+        part="snippet,replies",
+        videoId="l1LdFUFFRBQ",
+        pageToken = nextPageToken
+    )
+        response = request.execute()
+        returnedValues.append(response)
+        if x == maxPage:
+            nextPageToken=None
+        else:
+            nextPageToken = response['nextPageToken']
+    
+    listToWrite = json.dumps(returnedValues, indent=4)
+    with open("Output.json", "w") as outfile:
+        outfile.write(listToWrite)
 
     #getting comments
     #The scheme is "items" -> (now in list) "snippet" -> "topLevelComment" -> "snippet" -> "textDisplay"
     #Print the value of each snippet -> topLevelComment -> snippet -> textDisplay inside items[0]
-    lowerLevel = response['items']
     storedValue = ''
-    for i in range(0, len(lowerLevel)):
-        storedValue = storedValue + (str(i + 1) + ". " + str(lowerLevel[i]['snippet']['topLevelComment']['snippet']['textDisplay'])) + "\n"
+    for i in range(0, len(returnedValues)):
+        for j in range(0, len(returnedValues[i]['items'])):
+            storedValue = storedValue + (str(i * 20 + j + 1) + ". " + str(returnedValues[i]['items'][j]['snippet']['topLevelComment']['snippet']['textDisplay']) + "\n")
+        #storedValue = storedValue + (str(i + 1) + ". " + str(lowerLevel[i]['snippet']['topLevelComment']['snippet']['textDisplay'])) + "\n"
+    print(storedValue)
     #Not currently able to encode all of the comments for writing to ExtractedComments.txt file
 
     #adding this to write to Output file
